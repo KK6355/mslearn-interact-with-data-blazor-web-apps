7b49e8714ff72d16ebe97d3d905dc58d7738867f
vicky kang <kangjiawei666@126.com>
2024-01-17 12:00:21 +1300

    implemented place order

--- Data/PizzaStoreContext.cs
+++ Data/PizzaStoreContext.cs
@@ -7,4 +7,15 @@ public class PizzaStoreContext  : DbContext
     {
     }
     public DbSet<PizzaSpecial> Specials {get;set;}
+    public DbSet<Pizza> Pizzas {get;set;}
+    public DbSet<Order> Orders {get;set;}
+    public DbSet<Topping> Toppings {get;set;}
+    public DbSet<Address> Addresses {get;set;}
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<PizzaTopping>().HasKey(pst=>new {pst.PizzaId, pst.ToppingId});
+        modelBuilder.Entity<PizzaTopping>().HasOne<Pizza>().WithMany(ps=>ps.Toppings);
+        modelBuilder.Entity<PizzaTopping>().HasOne(pst=>pst.Topping).WithMany();
+    }
 }
--- Services/OrderState.cs
+++ Services/OrderState.cs
@@ -30,4 +30,8 @@ public class OrderState
     {
         Order.Pizzas.Remove(pizza);
     }
+    public void ResetOrder()
+    {
+        Order= new Order();
+    }
 }
